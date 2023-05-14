f988df04f5d2f1ee2c5a5a02a7d25d413608df63
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2023-05-14 13:57:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
 	for(let i=0;i<encodedStr.length;i++){
-		if(encodedStr[i] === undefined){
-			decodedArr.push(encodedStr[i]);
-		}
-		else{
+		// if(encodedStr[i] === undefined){
+		// 	decodedArr.push(encodedStr[i]);
+		// }
+		// else{
 			decodedArr.push(lookup[encodedStr[i]]);
-		}
+		// }
 	}
 
 	decodedArr.join("");
